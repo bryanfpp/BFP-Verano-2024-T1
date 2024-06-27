aaccbef7c3ee431e6d811b78070e283c872c55ad
bryan fuentes <bryanfp@dslb-178-006-003-195.178.006.pools.vodafone-ip.de>
2024-06-27 08:28:57 -0600

    Ejercicio:7, seccion b
    Signed-off-by: bryan fuentes <bryanfp@dslb-178-006-003-195.178.006.pools.vodafone-ip.de>

--- CodeBase7/dia_barras.py
+++ CodeBase7/dia_barras.py
@@ -1,14 +1,6 @@
 import matplotlib.pyplot as plt
 
 def generar_diagrama_barras(frecuancia,unique_names):
-    plt.title("Histograma", fontsize = 20)
-    plt.bar(unique_names, frecuancia)
-    plt.figure(figsize = (8, 4)) # (Ancho, alto) del grafico
-
-    # Diagrama de barras:
-    # - Barras al 100% (width = 1)
-    # - Color de contorno negro (edgecolor= "k")
-    # - Seleccionar color hexadecimal: https://htmlcolorcodes.com/es/
     plt.barh(unique_names,frecuancia ,
             height = 0.8, edgecolor= "k") 
 
